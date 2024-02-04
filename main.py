a6cc62e0360dbae92a05a3ec4ee3825ff21b8b42
shainhfl <vladshaindit@gmail.com>
2024-02-04 18:43:06 +0200

    added popups

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-
-from PySide6.QtWidgets import QApplication, QMainWindow
+import os
+from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
 from design import Ui_MainWindow
 
 class TextAnalizator(QMainWindow):
@@ -8,7 +8,35 @@ class TextAnalizator(QMainWindow):
         super(TextAnalizator, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-    
+        self.ui.btnOpenFile.clicked.connect(self.openFileButtonClicked)
+        self.ui.btnClear.clicked.connect(self.clearTextBrowser)
+        #self.ui.btnRun.clicked.connect(self.showMessageBox)
+
+    def openFileButtonClicked(self):
+        file_dialog = QFileDialog()
+        file_path, _ = file_dialog.getOpenFileName(self, 'Open File', '', 'All files (*)')
+
+        if file_path:
+            valid_extensions = ['.txt', '.doc', '.docx', '.rtf', '.pdf']
+            _, file_extension = os.path.splitext(file_path)
+            if file_extension.lower() in valid_extensions:
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    file_content = file.read()
+                    self.ui.textBrowser.setPlainText(file_content)
+            else:
+               self.showMessageBox()
+
+    def clearTextBrowser(self):
+        self.ui.textBrowser.clear()
+
+    def showMessageBox(self):
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle("ВЫ ДЕБИЛ")
+        msg_box.setText("ВЫ ДЕБИЛ")
+        msg_box.exec_()
+
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
